6f45a9fbfa7e29d69dd7db389bc82331879cca58
Greg <greg@mac.home>
2025-09-24 14:00:13 +0200

    try catch block added

--- 02Person.php
+++ 02Person.php
@@ -7,20 +7,24 @@
 
 class Person{
     public $name;
-    public $age;
-    //private $age;
+    //public $age;
+    private $age; // you need to use setAge to be able to acces this property
 
-    public function __construct($name) {
+    public function __construct($name)
+    {
         $this->name = $name;
     }
 
     public function setAge($age) {
+        try{
+            if($age < 18) {
+                throw new Exception('Person too young');
+            }
+            $this->age = $age;
 
-        if($age < 18) {
-            throw new Exception('Person too young');
+        }  catch (Exception $e) {
+            echo 'Message: ' .$e->getMessage();
         }
-        $this->age = $age;
-
     }
 
     public function getAge() {
@@ -29,12 +33,14 @@ class Person{
 }
 
 $john = new Person('John Doe');
-$john->setAge(30);
+$john->setAge(19);
 $john->getAge();
 
-// because age is public you can set it directly without using setAge() and check on age will not be done
-$john->age = 5;
+// if  age is public you can set it directly without using setAge() and check on age will not be done
+//$john->age = 5;
 // property $age needs to be private to not allow others to set prohibited values (age less then 18)
 // private $age
 
+echo '<pre>';
 var_dump($john);
+echo '<pre/>';
