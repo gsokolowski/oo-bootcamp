fa8ef26db63d3fe8e0f71b7accd73775d096a23d
Greg <greg@mac.home>
2025-09-24 14:28:01 +0200

    Ineritance

--- 04Inheritance.php
+++ 04Inheritance.php
@@ -12,12 +12,17 @@ abstract class Shape {
     private $color;
 
     public function __construct($color = 'red') {
-
+        // red by defalt on constructor
         return $this->color = $color;
     }
 
     public function getColor() {
         return $this->color;
+    }
+
+    // but you can override it using setColor()
+    public function setColor($color) {
+        $this->color = $color;
     }
 
     // abstract method in abstract class refers to :
@@ -44,7 +49,7 @@ class Triangle extends Shape {
     protected $base = 4;
     protected $height = 5;
 
-    // when  you need to override method getArea() from Parent Shape base class
+    // when  you need to override method getArea() from Parent Adstract Shape base class
     //  which was calculating only for square - just override it
 
     public function getArea() {
@@ -59,6 +64,8 @@ class Circle extends Shape {
 
     protected $radius = 7;
 
+        // when  you need to override method getArea() from Parent Adstract Shape base class
+    //  which was calculating only for square - just override it
     public function getArea() {
 
         return pi() * pow($this->radius, 2);
@@ -67,7 +74,7 @@ class Circle extends Shape {
 }
 
 
-// if Shape is and abstract class you can not instantiate it
+// if Shape is an abstract class you can not instantiate it
 // $square = new Shape(); // error
 
 $square = new Square('red');
@@ -75,18 +82,17 @@ echo $square->getArea().'<br />';
 echo $square->getColor().'<br />';
 
 
-
 $triangle = new Triangle();
+$triangle->setColor('blue'); // overriding construdtor colour to blue
 echo $triangle->getArea().'<br />';
+echo $triangle->getColor().'<br />';
 
 
 $circle = new Circle();
+$circle->setColor('yellow');
 echo $circle->getArea().'<br />';
+echo $circle->getColor().'<br />';
 
 
-
-
-
-
-
+echo 'square default color is still :'.$square->getColor().'<br />';
 
