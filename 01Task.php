946f8fa58015bbfc00313d8b9623659f5c71e251
Greg <greg@mac.home>
2025-09-24 13:39:28 +0200

    little update

--- 01Task.php
+++ 01Task.php
@@ -16,16 +16,21 @@ class Task {
         $this->description = $description;
 
     }
-
-    public function complete() {
+    
+    public function completed() {
         $this->completed = true;
     }
+    
+    public function notCompleted() {
+        $this->completed = false;
+    }
+    
 }
 
-$task = new Task('Learn OOP', 'This is description');
+$task = new Task('Title Learn OOP', 'This is description of the task');
 
-$task->complete();
-
+$task->completed();
+//$task->notCompleted();
 
 //var_dump($task->title);
 //var_dump($task->description);
