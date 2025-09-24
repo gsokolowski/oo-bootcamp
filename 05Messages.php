5ac84aa4661f2d0e61097a82a9665151ef4f738b
Greg <greg@mac.home>
2025-09-24 15:27:13 +0200

    oop messages improved

--- 05Messages.php
+++ 05Messages.php
@@ -18,21 +18,19 @@ class Person {
 
 }
 
-
 // Business hires a Person
 // Business depends on staff - in business has to be at least one person do force in construct a Stuff object
 class Business {
 
     protected $staff;
-    public function __construct(Staff $staff) {
+    public function __construct(Staff $staff) { // passing Staff through constructor
 
         $this->staff = $staff;
     }
 
-    public function hire(Person $person) {
+    public function hire(Person $person) { // add Person to the staff collection
 
-        // add person to the staff collection
-        $this->staff->add($person);
+        $this->staff->add($person); // $this->staff is a collection
     }
 
     public function getStaffMembers() {
@@ -41,18 +39,16 @@ class Business {
     }
 }
 
-
-// Person joins the Staff
+// Person joins the Staff of the business
 class Staff {
-    protected $members = [];
+    protected $members = []; // is will be an array of Person objects
 
     public function __construct($members = []){
         $this->members = $members;
-
     }
 
     public function add(Person $person) {
-        $this->members[] = $person;
+        $this->members[] = $person; // adding Person object to and array
     }
 
     public function getMembers() {
@@ -61,27 +57,55 @@ class Staff {
 
 }
 
-// we have a person
-$jeffrey = new Person('Jeffrey Way');
-// we have a staff
-$staff = new Staff( [$jeffrey] );
-// we have a business
-$laracast = new Business($staff);
 
-// Business is going to hire jeffrey
-// $laracast->hire($jeffrey);
+
+
+
+
+// we have a person
+$person = new Person('Jeffrey Way');
+
+echo '<pre>';
+var_dump($person);
+echo '</pre>';
+
+
+// we have a staff and adding person object into Staff array propert called member through constructor
+$staff = new Staff( [$person] );
+echo '<pre>';
+var_dump($staff);
+echo '</pre>';
+
+
+// finally we have a business and we add staff object into businnes class staff collection
+$business = new Business($staff); 
+
+// Business is going to hire new guy Greg
+$person = new Person('Greg Sokolowski');
+$business->hire($person);
+
+echo '<pre>';
+var_dump($business);
+echo '</pre>';
 
 // lets hire sommone else
-$laracast->hire(new Person('Jane Doe'));
+$business->hire(new Person('Jane Doe'));
 
 echo '<pre>';
-var_dump($laracast->getStaffMembers());
+var_dump($business);
+echo '</pre>';
+
+
+// How to get all members of that busness?
+echo '<pre>';
+var_dump($business->getStaffMembers());
 echo '<pre/>';
+
 
 echo '<pre>';
-//var_dump($staff);
+var_dump($staff);
 echo '<pre/>';
 
-
+die();
 
 
